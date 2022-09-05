a7f065adf8331b5ab13efb7c72206c4e2a73d0dd
Chrysz <csotorepol@gmail.com>
2022-09-05 15:29:56 -0400

    Se agrega carro de compra y manejo con los distintos menus

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,12 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 navEmail.addEventListener('click', toggleDesktopMenu);
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
+    const isMenuCartIcon = menuCartIcon.classList.contains('inactive');
+
+    if (!isMenuCartIcon)
+        menuCartIcon.classList.toggle('inactive');
+
     desktopMenu.classList.toggle('inactive');
 }
 
@@ -10,6 +15,28 @@ function toggleDesktopMenu(){
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-function toggleMobileMenu(){
+function toggleMobileMenu() {
+    const isMenuCartIcon = menuCartIcon.classList.contains('inactive');
+
+    if (!isMenuCartIcon)
+        menuCartIcon.classList.toggle('inactive');
+
     mobileMenu.classList.toggle('inactive');
+}
+
+// Shopping cart menu
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const menuCartIcon = document.querySelector('.product-detail');
+shoppingCart.addEventListener('click', toggleShoppingCartMenu);
+function toggleShoppingCartMenu() {
+    const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClose = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClose)
+        mobileMenu.classList.toggle('inactive');
+    
+    if(!isDesktopMenuClose)
+        desktopMenu.classList.toggle('inactive');
+
+    menuCartIcon.classList.toggle('inactive');
 }
