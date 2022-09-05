3a53a2a91cc9530d02371c3cd5c5ffa3dfe2c1d0
Chrysz <csotorepol@gmail.com>
2022-09-05 15:01:44 -0400

    se agrega menu mobile

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+// Desktop menu
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 navEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+// Mobile menu
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
