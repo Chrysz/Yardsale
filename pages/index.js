2a4a323ae3cfe7ec1f1a58912131952b46bc3783
Chrysz <csotorepol@gmail.com>
2022-09-05 21:52:47 -0400

    Se agrega lista de productos con funcion de render en js

--- pages/index.js
+++ pages/index.js
@@ -34,9 +34,52 @@ function toggleShoppingCartMenu() {
 
     if (!isMobileMenuClose)
         mobileMenu.classList.toggle('inactive');
-    
-    if(!isDesktopMenuClose)
+
+    if (!isDesktopMenuClose)
         desktopMenu.classList.toggle('inactive');
 
     menuCartIcon.classList.toggle('inactive');
 }
+
+const cardsContainer = document.querySelector('.cards-container');
+
+// Product list
+const productList = [
+    { name: 'Bike', price: 120, image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?cs=srgb&dl=pexels-philipp-m-100582.jpg&fm=jpg' },
+    { name: 'Bike', price: 121, image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?cs=srgb&dl=pexels-philipp-m-100582.jpg&fm=jpg' },
+    { name: 'Bike', price: 122, image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?cs=srgb&dl=pexels-philipp-m-100582.jpg&fm=jpg' },
+    { name: 'Bike Novice', price: 123, image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?cs=srgb&dl=pexels-philipp-m-100582.jpg&fm=jpg' },
+    { name: 'Bike Medium', price: 124, image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?cs=srgb&dl=pexels-philipp-m-100582.jpg&fm=jpg' },
+    { name: 'Bike Pro', price: 125, image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?cs=srgb&dl=pexels-philipp-m-100582.jpg&fm=jpg' },
+];
+
+function renderProducts(arrObject){
+    for (prod of arrObject) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        const productImage = document.createElement('img');
+        productImage.setAttribute('src', prod.image);
+        
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        
+        const productInfoDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$'+prod.price;
+        const productName = document.createElement('p');
+        productName.innerText = prod.name;
+        
+        const productInfoFigure = document.createElement('figure');
+        const productFigureImgCart = document.createElement('img');
+        productFigureImgCart.setAttribute('img', '../icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productFigureImgCart);
+        productInfoDiv.append(productPrice, productName);
+        productInfo.append(productInfoDiv, productInfoFigure);
+    
+        productCard.append(productImage, productInfo);
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
